9a2e31e377ffb6c321ca20c7fadbf25db9d53788
GolovanovAlex <Golovanov.a.a@yandex.ru>
2022-06-20 17:51:33 +0300

    let js

--- app.js
+++ app.js
@@ -2,3 +2,5 @@ let stars = document.getElementById('stars');
 let moon = document.getElementById('moon');
 let mountainsBehind = document.getElementById('mountains_behind');
 let mountainsFront = document.getElementById('mountains_front');
+let btn = document.getElementById('btn');
+let title = document.getElementById('text');
