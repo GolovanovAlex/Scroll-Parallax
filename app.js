c8bb7678ccfeab9a581b9a5420142e76cd1f8712
GolovanovAlex <Golovanov.a.a@yandex.ru>
2022-06-20 17:59:01 +0300

    js

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ let mountainsBehind = document.getElementById('mountains_behind');
 let mountainsFront = document.getElementById('mountains_front');
 let btn = document.getElementById('btn');
 let title = document.getElementById('text');
+
+window.addEventListener('scroll', function () {
+  let value = window.scrollY;
+  stars.style.left = value * 0.25 + 'px';
+  moon.style.right = value + 'px';
+});
